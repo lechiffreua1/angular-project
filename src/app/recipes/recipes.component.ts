81eb8b043b97f95cbfc66f7e138e1a94aba924c0
le_chiffre <le.chiffreua1gmail.com>
2017-04-28 10:39:23 +0300

    delete active, inactive components

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,31 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
-import {UsersService} from "./users.service";
-import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css'],
-  providers: [UsersService, CounterService]
+  styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
   selected: Recipe;
-  activeUsers: string[] = [];
-  inactiveUsers: string[] = [];
-  countActive: number;
-  countInactive: number;
 
-  constructor(private usersService: UsersService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.activeUsers = this.usersService.activeUsers;
-    this.inactiveUsers = this.usersService.inactiveUsers;
-    this.countActive = this.usersService.countActive;
-    this.countInactive = this.usersService.countInactive;
-
-    console.log(this.countActive, this.countInactive);
   }
 
   showDetailRecipe(recipe: Recipe) {
