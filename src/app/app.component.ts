03a7a644c8aa329372f9742b70767d0da0eb2100
lechiffreua1 <le.chiffreua1@gmail.com>
2017-05-04 21:51:17 +0300

    add angular router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showRecipes: boolean = true;
-
-  onChangeView(recipe: string) {
-    this.showRecipes = recipe === 'recipe' ? true: false;
-  }
 
 }
